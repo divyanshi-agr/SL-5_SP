73266d434b34f07f6a3c86faec3e8fdb7a86c676
Divyanshi Agrawal <divsagr3@gmail.com>
2021-06-09 22:13:25 +0530

    adding final sp assignments

---
+++ A4_lexAnalyzer.cpp
@@ -0,0 +1,280 @@
+#include<bits/stdc++.h>
+#include <typeinfo>
+using namespace std;
+
+int line_number = 0;
+
+struct ust_info {
+    string token;
+    string token_class;
+    int index;
+};
+
+struct terminal {
+	string indicator;
+    int index;
+};
+
+struct identifier {
+    string id_name;
+    int index;  
+};
+
+struct literal {
+    string lit_name;
+    int index;
+};
+
+vector<identifier> identifier_table;
+vector<literal> literal_table;
+vector<ust_info> ust_table;
+unordered_map<string, terminal> terminal_table;
+
+void initialize(unordered_map<string, terminal> &trm_table)
+{
+    trm_table["int"] = {"key", 1};
+    trm_table["float"] = {"key", 2};
+    trm_table["double"] = {"key", 3};
+    trm_table["char"] = {"key", 4};
+    trm_table["struct"] = {"key", 5};
+    trm_table["if"] = {"key", 6};
+    trm_table["else"] = {"key", 7};
+    trm_table["return"] = {"key", 8};
+    trm_table["for"] = {"key", 9};
+    trm_table["do"] = {"key", 10};
+    trm_table["while"] = {"key", 11};
+    trm_table["+"] = {"op", 12};
+    trm_table["-"] = {"op", 13};
+    trm_table["*"] = {"op", 14};
+    trm_table["/"] = {"op", 15};
+    trm_table["%"] = {"op", 16};
+    trm_table["="] = {"op", 17};
+    trm_table["<"] = {"op", 18};
+    trm_table[">"] = {"op", 19};
+    trm_table["!"] = {"op", 20};
+    trm_table["&"] = {"op", 21};
+    trm_table["|"] = {"op", 22};
+    trm_table["?"] = {"op", 23};
+    trm_table[":"] = {"op", 24};
+    trm_table["+"] = {"op", 25};
+    trm_table["^"] = {"op", 26};
+    trm_table["("] = {"BC", 27};
+    trm_table[")"] = {"BC", 28};
+    trm_table["{"] = {"BC", 29};
+    trm_table["}"] = {"BC", 30};
+    trm_table[","] = {"BC", 31};
+    trm_table[";"] = {"BC", 32};
+}
+
+bool isSymbol(char c)
+{
+    if(((int)c >= 32 && (int)c <= 47) || ((int)c >= 58 && (int)c <= 64)
+        || ((int)c >= 91 && (int)c <= 96) || ((int)c >= 123 && (int)c <= 126))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool isNo(char c)
+{
+    if(c >= '0' && c <= '9')
+    {
+        return true;
+    }
+    return false;
+}
+
+void get_words(){
+
+    FILE* fp=fopen("sample.txt","r");
+    char c;
+    
+    char buffer[100];
+    int k=0;
+    int idIndex = 1, litIndex = 1; 
+
+    
+    while((c=fgetc(fp))!=EOF){
+        if(c=='\n'){
+            line_number++;
+            buffer[k]=0;
+            k=0;
+            int word_c=0;
+            char ans[100];
+            int p=0;
+            int i=0;
+    int f=0;
+                    
+
+            //check if more than 1 spaces, then take only 1 space
+            while(buffer[i]){
+                if(buffer[i]==' ' || buffer[i]=='\t'){
+                    if(f){
+                        ans[p]=' ';
+                        p++;
+                    }
+                    while(buffer[i]==' ' || buffer[i]=='\t')
+                        i++; 
+                }
+                else{
+                    ans[p]=buffer[i];
+                    f=1;
+                    p++;i++;
+                }
+            }
+            i=0;
+            ans[p]=0;
+            
+            //buffer stores 1 line of file
+            //ans stores 1 line of file without extra spaces
+            if(ans[0]==' ')word_c--;
+            if(ans[p-1]==' ')word_c--;  
+            string temp;
+            string op;
+            vector<string> words;
+            vector<string> lits;
+            vector<string>::iterator it;
+            
+            //inserting token in words array
+            while(ans[i]){                          
+                //for special characters             
+                if(isSymbol(ans[i]))
+                {
+                    //for checking literal
+                    if(ans[i] == '"')
+                    {
+                        temp += ans[i];     //store first " in temp
+                        i++;
+                        while(ans[i] != '"')
+                        {
+                            temp += ans[i]; //store everything inside "" in temp
+                            i++;
+                        }
+                        temp += ans[i];     //store last " in temp
+                        words.push_back(temp);
+                        lits.push_back(temp);
+                        temp = "";
+                    }
+                    //if any other symbol occurs, which is not a space
+                    else if(ans[i] != ' ')
+                    {
+                        temp += ans[i];
+                        words.push_back(temp);
+                        temp = "";
+                    }
+                    
+                }
+                //for number and lookahead also number
+                else if(isdigit(ans[i]) && isdigit(ans[i+1]))
+                {
+                    temp += ans[i];
+                }
+                //for number and lookahead a symbol
+                else if(isdigit(ans[i]) && isSymbol(ans[i+1]))
+                {
+                    temp += ans[i];
+                    words.push_back(temp);
+                    lits.push_back(temp);
+                    temp = "";
+                }
+                //for alphabet and lookahead also alphabet
+                else if(isalpha(ans[i]) && isalpha(ans[i+1]))
+                    temp += ans[i];
+                
+                //for alphabet and lookahead not alphabet but symbol
+                else if(isalpha(ans[i]) && isSymbol(ans[i+1]))
+                {
+                    temp += ans[i];
+                    words.push_back(temp);
+                    temp = "";
+                }
+                //error handling
+                else
+                {
+                    std::cout<<endl<<"Line number "<<line_number<<" has syntax error --> "<<ans[i]<<ans[i+1]<<endl;
+                }
+
+                i++;
+            }
+ 
+            //distributing in respective tables
+            for(auto x:words){
+                
+                //if token is a terminal, just push it in UST table
+                if(terminal_table.find(x)!=terminal_table.end()){   
+                   ust_table.push_back({x, "Terminal", terminal_table[x].index});
+                }
+
+                 //if the token is not found in terminal table
+                if(terminal_table.find(x)==terminal_table.end()){ 
+
+                    it = find(lits.begin(), lits.end(), x );
+                    //if token is a literal
+                    if (it != lits.end())
+                    {
+                        ust_table.push_back({x, "A Literal", litIndex});
+                        literal_table.push_back({x, litIndex});
+                        litIndex++;
+                    }
+                    //if token is an identifier
+                    else {
+                        ust_table.push_back({x, "Identifier", idIndex});
+                        identifier_table.push_back({x, idIndex});
+                        idIndex++;
+                    }
+                } 
+            }       
+    }
+    else{
+            if(isdigit(c) || c=='=' || isalpha(c) || c==' ' || c=='\t' || isSymbol(c)){
+                buffer[k]=c;
+                k++;
+            }
+            else{
+                buffer[k]=' ';
+                k++;
+            }
+        }
+    }
+    
+    fclose(fp);     
+}
+
+int main() { 
+   initialize(terminal_table);
+    get_words();
+
+    std::cout<<endl<<endl<<"UST TABLE ::";
+    std::cout<<endl<<"----------------------------------";
+    std::cout<<endl<<"TOKEN"<<"\t"<<"CLASS"<<"\t\t"<<"INDEX"<<endl;
+    std::cout<<"----------------------------------"<<endl;
+
+        for(auto x:ust_table){
+            std::cout<<x.token<<"\t"<<x.token_class<<"\t"<<x.index<<endl;
+        
+        }
+
+    std::cout<<endl<<endl<<"IDENTIFIER TABLE ::";
+    std::cout<<endl<<"----------------------------------";
+    std::cout<<endl<<"IDENTIFIER"<<"\t"<<"INDEX";
+    std::cout<<endl<<"----------------------------------"<<endl;
+
+        for(auto x:identifier_table){
+            std::cout<<x.id_name<<"\t\t"<<x.index<<endl;
+        }
+
+    std::cout<<endl<<endl<<"LITERAL TABLE ::";
+    std::cout<<endl<<"----------------------------------";
+    std::cout<<endl<<"LITERAL"<<"\t\t"<<"INDEX"<<endl;
+    std::cout<<"----------------------------------"<<endl;
+
+        for(auto x:literal_table){
+            std::cout<<x.lit_name<<"\t\t"<<x.index<<endl;
+        }
+
+    std::cout<<endl;
+    return 0;
+}
+
---
+++ A5_rdp.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+int S();
+int E();
+int T();
+int E1();
+int T1();
+int F();
+void match(char);
+
+//Grammar 
+/*
+S->E
+E->TE1
+T->FT1
+E1-> +TE1 | -TE1 | null
+T1-> *FT1 | /FT1 | null
+F-> no | alphabet
+*/
+string input;
+int i=0;
+int main()
+{
+    cout<<"Enter input string: ";
+    getline(cin,input);
+    int err=S();
+    if(err==1 && i==input.length())
+    {
+        cout<<"Accepted\n";
+    }
+    else
+    {
+        cout<<"Rejected\n";
+    }
+    return 0;
+}
+
+int S()
+{
+    cout<<"S\n";
+    int err1=E();
+    if(err1==-1)
+    {
+        return -1;
+    }
+    return 1;
+}
+int E()
+{
+    cout<<"E\n";
+    int err1=T();
+    int err2=E1();
+    if(err1==-1 || err2==-1)
+    {
+        return -1;
+    }
+    return 1;
+}
+int T()
+{
+    cout<<"T\n";
+    int err1=F();
+    int err2=T1();
+    if(err1==-1 || err2==-1)
+    {
+        return -1;
+    }
+    return 1;
+}
+int E1()
+{
+    cout<<"E1\n";
+    if(input[i]=='+')
+    {
+        i++;
+        int err1=T();
+        int err2=E1();
+        if(err1==-1 || err2==-1)
+        {
+            return -1;
+        }
+    }
+    else if(input[i]=='-')
+    {
+       i++;
+        int err1=T();
+        int err2=E1();
+        if(err1==-1 || err2==-1)
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+    return 1;
+    
+}
+int T1()
+{
+    cout<<"T1\n";
+    if(input[i]=='*')
+    {
+       i++;
+        int err1=F();
+        int err2=T1();
+        if(err1==-1 || err2==-1)
+        {
+            return -1;
+        }
+    }
+    else if(input[i]=='/')
+    {
+        i++;
+        int err1=F();
+        int err2=T1();
+        if(err1==-1 || err2==-1)
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+    return 1;
+    
+}
+
+int F()
+{
+    cout<<"F\n";
+    if(input[i]>='0' && input[i]<='9')
+    {
+       i++;
+        return 1;
+    }
+    else if(input[i]>='a' && input[i]<='z')
+    {
+       i++;
+        return 1;
+    }
+    return -1;
+}
+
